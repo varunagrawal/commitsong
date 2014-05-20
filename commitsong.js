fe435e77e12d7b7b962031fd69b2d4c3ce45f011
Varun Agrawal <varagrawal@gmail.com>
2014-05-20 18:27:46 +0530

    get access token

--- commitsong.js
+++ commitsong.js
@@ -1,13 +1,26 @@
+CLIENT_ID = "b0cdce6d049d97e8d1b3";
+CLIENT_SECRET = "109e825552093e60fab8810fd59c8855d4740bfa";
+
 $( document ).ready( function(){
     $("#login").click( function(){
 	login();
     });
-    main();
+    
+    var result = getURLParameters(document.URL);
+
+    alert(result["code"]);
+
+    if(result["code"] != null)
+    {
+	var code = getToken(result["code"]);
+	
+	main();
+    }
 });
 
 function main(){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.github.com/users/varunagrawal", false);
+    xhr.open("GET", "https://api.github.com/user", false);
     xhr.send();
     
     var obj = jQuery.parseJSON(xhr.responseText);
@@ -16,7 +29,42 @@ function main(){
 };
 
 function login(){
-    window.location.replace("https://github.com/login/oauth/authorize?client_id=b0cdce6d049d97e8d1b3&redirect_uri=http://varunagrawal.github.io/commitsong/&scope=user,user:email,public_repo,repo:status")
-  //  window.open("login","Github login","width=550,height=170,left=150,top=200,toolbar=0,status=0");
+    window.location.replace("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + "&redirect_uri=http://varunagrawal.github.io/commitsong/&scope=user,user:email,public_repo,repo:status")
 };
 
+
+function getURLParameters(url){
+
+    var result = {};
+    var searchIndex = url.indexOf("?");
+    if (searchIndex == -1 ) return result;
+    var sPageURL = url.substring(searchIndex +1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {       
+        var sParameterName = sURLVariables[i].split('=');      
+        result[sParameterName[0]] = sParameterName[1];
+    }
+    return result;
+}
+
+function getToken(code){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://github.com/login/oauth/access_token?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + code + "redirect_uri=http://varunagrawal.github.io/commitsong/", false);
+
+    xhr.send();
+    
+    var arr = xhr.responseText;
+    var res = $.map( arr, function(n, i){
+	return n.split("=");
+    });
+
+    var result = null;
+    for(var i=0; i<res.length; i++){
+	if(res[i] == "code"){
+	    result = res[i+1];
+	}
+    }
+    
+    return result;
+}
