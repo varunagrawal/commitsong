3ae03ae95e3b19ce94b31a89b7d7e5fd1eed66a9
Varun Agrawal <varagrawal@gmail.com>
2014-05-20 14:42:46 +0530

    Set up basic page

---
+++ commitsong.js
@@ -0,0 +1,22 @@
+$( document ).ready( function(){
+    $("#login").click( function(){
+	login();
+    });
+    main();
+});
+
+function main(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/users/varunagrawal", false);
+    xhr.send();
+    
+    var obj = jQuery.parseJSON(xhr.responseText);
+
+    $("#github").html(xhr.responseText + obj.name);
+};
+
+function login(){
+    window.location.replace("https://github.com/login/oauth/authorize?client_id=b0cdce6d049d97e8d1b3&redirect_uri=https://www.google.com&scope=user,user:email,public_repo,repo:status")
+  //  window.open("login","Github login","width=550,height=170,left=150,top=200,toolbar=0,status=0");
+};
+
