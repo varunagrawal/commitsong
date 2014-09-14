8bd6590853e29a915ef5c8b9f2c2e761e9f54787
Varun Agrawal <varagrawal@gmail.com>
2014-09-14 22:56:49 +0530

    auth test

--- utils.js
+++ utils.js
@@ -10,7 +10,7 @@ function request(url, type, data, onSuccess, onError, contentType){
     $.ajax({
 	type: type,
 	url: url,
-	headers: { 'Accept': 'application/vnd.github.v3+json', 'Origin': 'http://www.varunagrawal.me' },
+	headers: { 'Accept': 'application/vnd.github.v3+json', 'Origin': 'http://www.varunagrawal.me/commitsong' },
 	/*beforeSend: function(xhr){
 	    xhr.setRequestHeader('Origin', 'http://www.varunagrawal.me');
 	    xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
