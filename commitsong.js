6c23b07c1f730fa9b66569f74d20655a059a32fc
Varun Agrawal <varagrawal@gmail.com>
2014-09-17 01:53:44 +0530

    Added moment datetime functionality

--- commitsong.js
+++ commitsong.js
@@ -6,7 +6,7 @@ $( document ).ready( function(){
 });
 
 function commits(data){
-    getCommits('varunagrawal', data[0].name, display, errorMessage);
+    getCommits('varunagrawal', 'ShowOfHands', display, errorMessage, 'varunagrawal', '2013-09-17T01:09:12+05:30');
     /*for(var i=0; i< data.length; i++){
 	alert(data[i].name);
 
@@ -14,7 +14,25 @@ function commits(data){
 }
 
 function display(data){
-    $('#data').text(JSON.stringify(data));
+
+    var now = moment();
+    var oneYearAgo = moment().subtract(1, 'years'); 
+
+    var commitData = []
+    var size = now.diff(oneYearAgo, 'days');
+    for(var i=0; i<size; i++) commitData[i] = 0;
+
+    for(var i=0; i<data.length; i++){
+	
+	var commitDate = moment(data[i].commit.committer.date);
+	if(commitDate.isBefore(now) && commitDate.isAfter(oneYearAgo)){
+	    //alert(commitDate.diff(oneYearAgo, 'days'));
+	    commitData[commitDate.diff(oneYearAgo, 'days')] += 1;
+	}
+
+    }
+    
+    $('#data').text(commitData);
 }
 
 function errorMessage(){
@@ -27,6 +45,6 @@ function getRepos(username, onSuccess, onError){
     request(githubAPI + "/users/" + username + "/repos", 'GET', {}, onSuccess, onError);
 }
 
-function getCommits(username, repo, onSuccess, onError){
-    request(githubAPI + "/repos/" + username + "/" + repo + "/commits", 'GET', {}, onSuccess, onError);
+function getCommits(username, repo, onSuccess, onError, author, since){
+    request(githubAPI + "/repos/" + username + "/" + repo + "/commits", 'GET', {author: author, since: since}, onSuccess, onError);
 }
